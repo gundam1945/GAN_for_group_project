f57a37fbadda6bd991cb5cf8504528a5f3b04be3
Jim Yu <jimyu@multibase.com.hk>
2022-04-08 14:15:49 +0800

    folder structure change

--- src/pytorch/model1.py
+++ src/pytorch/model1.py
@@ -15,15 +15,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-# MNIST 214
 
 class Model(object):
-    def __init__(self, opt):
+    def __init__(self, opt, dir_name):
         self.img_shape = (opt.channels, opt.img_size, opt.img_size)
-        dir = "\\state\\gan6"
-        os.makedirs(os.path.abspath(os.path.curdir) + dir, exist_ok=True)
-        self.PATH_G = os.path.abspath(os.path.curdir) + dir + "\\generator.pth"
-        self.PATH_D = os.path.abspath(os.path.curdir) + dir + "\\discriminator.pth"
+        dir_name = "\\state\\gan1"
+        os.makedirs(os.path.abspath(os.path.curdir) + dir_name, exist_ok=True)
+        self.PATH_G = os.path.abspath(os.path.curdir) + dir_name + "\\generator.pth"
+        self.PATH_D = os.path.abspath(os.path.curdir) + dir_name + "\\discriminator.pth"
         self.generator = Model.Generator(opt)
         self.discriminator = Model.Discriminator(opt)
 
@@ -45,7 +44,7 @@ class Model(object):
                 *block(256, 512),
                 *block(512, 1024),
                 nn.Linear(1024, int(np.prod(self.img_shape))),
-                nn.Sigmoid()
+                nn.Tanh()
             )
 
         def forward(self, z):
@@ -57,18 +56,13 @@ class Model(object):
         def __init__(self, opt):
             super(Model.Discriminator, self).__init__()
             self.img_shape = (opt.channels, opt.img_size, opt.img_size)
-            dropout_p = 0.4
 
             self.model = nn.Sequential(
                 nn.Linear(int(np.prod(self.img_shape)), 512),
                 nn.LeakyReLU(0.2, inplace=True),
-                nn.Dropout(p=dropout_p),
                 nn.Linear(512, 256),
                 nn.LeakyReLU(0.2, inplace=True),
-                nn.Dropout(p=dropout_p),
-                nn.Linear(256, 25),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Linear(25, 1),
+                nn.Linear(256, 1),
                 nn.Sigmoid(),
             )
 
@@ -86,7 +80,7 @@ def get_dataloader(opt):
             train=True,
             download=True,
             transform=transforms.Compose(
-                [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.0], [1.0])]
+                [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
             ),
         ),
         batch_size=opt.batch_size,
--- src/pytorch/model6.py
+++ src/pytorch/model6.py
@@ -15,14 +15,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+# MNIST 214
 
 class Model(object):
     def __init__(self, opt):
         self.img_shape = (opt.channels, opt.img_size, opt.img_size)
-        dir = "\\state\\gan1"
-        os.makedirs(os.path.abspath(os.path.curdir) + dir, exist_ok=True)
-        self.PATH_G = os.path.abspath(os.path.curdir) + dir + "\\generator.pth"
-        self.PATH_D = os.path.abspath(os.path.curdir) + dir + "\\discriminator.pth"
+        dir_name = "\\state\\gan6"
+        os.makedirs(os.path.abspath(os.path.curdir) + dir_name, exist_ok=True)
+        self.PATH_G = os.path.abspath(os.path.curdir) + dir_name + "\\generator.pth"
+        self.PATH_D = os.path.abspath(os.path.curdir) + dir_name + "\\discriminator.pth"
         self.generator = Model.Generator(opt)
         self.discriminator = Model.Discriminator(opt)
 
@@ -44,7 +45,7 @@ class Model(object):
                 *block(256, 512),
                 *block(512, 1024),
                 nn.Linear(1024, int(np.prod(self.img_shape))),
-                nn.Tanh()
+                nn.Sigmoid()
             )
 
         def forward(self, z):
@@ -56,13 +57,18 @@ class Model(object):
         def __init__(self, opt):
             super(Model.Discriminator, self).__init__()
             self.img_shape = (opt.channels, opt.img_size, opt.img_size)
+            dropout_p = 0.4
 
             self.model = nn.Sequential(
                 nn.Linear(int(np.prod(self.img_shape)), 512),
                 nn.LeakyReLU(0.2, inplace=True),
+                nn.Dropout(p=dropout_p),
                 nn.Linear(512, 256),
                 nn.LeakyReLU(0.2, inplace=True),
-                nn.Linear(256, 1),
+                nn.Dropout(p=dropout_p),
+                nn.Linear(256, 25),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Linear(25, 1),
                 nn.Sigmoid(),
             )
 
@@ -80,7 +86,7 @@ def get_dataloader(opt):
             train=True,
             download=True,
             transform=transforms.Compose(
-                [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+                [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.0], [1.0])]
             ),
         ),
         batch_size=opt.batch_size,
